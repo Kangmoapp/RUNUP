36028e877f03b52345c6ca0f1b196254cfd84b0a
Kangmoapp <2001kys@gmail.com>
2026-02-26 17:41:50 +0900

    # 각종 Usecase 구현

--- app/src/main/java/com/example/runup/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/com/example/runup/domain/usecase/LoginUseCase.kt
@@ -1,12 +1,13 @@
 package com.example.runup.domain.usecase
 
+import com.example.runup.domain.model.AuthResult
 import com.example.runup.domain.model.UserLoginInfo
 import com.example.runup.domain.repository.UserRepository
 
 class LoginUseCase(
     private val userrepository: UserRepository
 ) {
-    suspend operator fun invoke(userlogininfo: UserLoginInfo) : String{
-        return "test"
+    suspend operator fun invoke(userlogininfo: UserLoginInfo) : AuthResult<Boolean>{
+        return userrepository.login(userlogininfo.userEmail, userlogininfo.userPw)
     }
 }
--- app/src/main/java/com/example/runup/domain/usecase/RecordRunningUseCase.kt
+++ app/src/main/java/com/example/runup/domain/usecase/RecordRunningUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.runup.domain.usecase
 
+import com.example.runup.domain.model.AuthResult
 import com.example.runup.domain.model.RunRecord
 import com.example.runup.domain.repository.CourseRepository
 import com.example.runup.domain.repository.UserRepository
@@ -9,7 +10,7 @@ class RecordRunningUseCase(
     private val courserepository: CourseRepository
     //user DB와 course DB 두곳에 전부 코스를 저장해야함
 ) {
-    suspend operator fun invoke(runrecord : RunRecord){
-
+    suspend operator fun invoke(runrecord : RunRecord) : AuthResult<Boolean> {
+        return userRepository.saveRunRecord(runrecord)
     }
 }
