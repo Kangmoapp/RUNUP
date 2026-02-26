e962009d60336f02d657216c28f313e5c1566d61
Kangmoapp <2001kys@gmail.com>
2026-02-26 17:43:15 +0900

    # Dataclass 기본 반환형 추가, userId String으로 변경, LocationPoint 삭제

--- app/src/main/java/com/example/runup/domain/model/UserLoginInfo.kt
+++ app/src/main/java/com/example/runup/domain/model/UserLoginInfo.kt
@@ -1,17 +1,17 @@
 package com.example.runup.domain.model
 
 data class UserLoginInfo(
-    val userId: Int,
+    val userId: String,
     val userEmail: String,
     val userPw: String
 )
 
 data class UserData(
-    val userId: Int,
-    val userEmail: String,
-    val userPassword: String,
-    val userName: String,
-    val goalDistance: Int, // 목표 거리 (meter 단위)
-    val goalTime: Int,     // 목표 시간 (milliseconds 단위)
-    val runs: List<RunRecord> // 사용자의 전체 러닝 기록 리스트
+    val userId: String = "",
+    val userEmail: String = "",
+    val userPassword: String = "",
+    val userName: String = "",
+    val goalDistance: Int = 0, // 목표 거리 (meter 단위)
+    val goalTime: Int = 0,     // 목표 시간 (milliseconds 단위)
+    val runs: List<RunRecord> = emptyList()// 사용자의 전체 러닝 기록 리스트
 )
