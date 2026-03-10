c98bfe780c322cbd4d46bbfc2fa5dee3c829ccd7
Kangmoapp <2001kys@gmail.com>
2026-03-10 12:01:03 +0900

    # data class Node, Scores 추가 Course 수정 각 GeoPoint 를 Node로 변경하여 코스 자체의 특징 점수 추가

--- app/src/main/java/com/example/runup/domain/model/RunRecord.kt
+++ app/src/main/java/com/example/runup/domain/model/RunRecord.kt
@@ -11,9 +11,21 @@ data class RunRecord(
 data class Course(
     val id: String = "1",
     val distance: Int = 500,
-    val locationPoints: List<GeoPoint> = listOf(),
+    val locationPoints: List<Node> = listOf(),
     val minLat: Double = 0.0, // 최남단
     val maxLat: Double = 0.0, // 최북단
     val minLng: Double = 0.0, // 최서단
     val maxLng: Double = 0.0, // 최동단
+    val scores: Scores = Scores(0.0,0.0,0.0)
+)
+
+data class Node(
+    val locationPoint: GeoPoint = GeoPoint(35.0,128.0),
+    val score : Scores = Scores(0.0, 0.0, 0.0)
+)
+
+data class Scores(
+    val brightScore: Double = 0.0,
+    val crowdedScore: Double = 0.0,
+    val hardScore: Double = 0.0,
 )
