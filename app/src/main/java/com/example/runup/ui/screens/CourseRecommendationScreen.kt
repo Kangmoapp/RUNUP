76b9621470aad5b3d0ad5cf9038e43bc39e0d0c4
sh <xyb456@naver.com>
2026-03-25 23:44:01 +0900

    코스 추천 화면 구성

--- app/src/main/java/com/example/runup/ui/screens/CourseRecommendationScreen.kt
+++ app/src/main/java/com/example/runup/ui/screens/CourseRecommendationScreen.kt
@@ -1,8 +1,10 @@
 package com.example.runup.ui.screens
 
+import android.app.Dialog
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,39 +22,83 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.runup.domain.model.SortType
+import com.example.runup.ui.components.DistanceGoalSettingDialog
 import com.example.runup.ui.components.TopBar
 import com.example.runup.ui.theme.BackGroudColor
+import com.example.runup.ui.theme.Black
 import com.example.runup.ui.theme.Gray
 import com.example.runup.ui.theme.PointColor
 import com.example.runup.ui.theme.TextBlack
 import com.example.runup.ui.theme.TextGray
 import com.example.runup.ui.theme.TextWhite
 import com.example.runup.ui.theme.White
+import com.example.runup.viewmodel.CourseRecommendationUiState
+import com.example.runup.viewmodel.CourseRecommendationViewModel
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
 
 @Composable
 fun CourseRecommendationScreen(
     onBackClick:()->Unit,
-    onMenuClick:()->Unit
+    onMenuClick:()->Unit,
+    viewModel: CourseRecommendationViewModel = hiltViewModel()
 ){
+    val uiState by viewModel.uiState.collectAsState()
+
     CourseRecommendationContent(
+        uiState = uiState,
+        onDistanceClick = {viewModel.openDistanceDialog()},
+        onDistanceClose = {viewModel.closeDistanceDialog()},
+        onDistanceConfirm = {viewModel.confirmDistance(it)},
+        onSortClick = {viewModel.openSortDialog()},
+        onSortClose = {viewModel.closeSortDialog()},
+        onSortConfirm = { sort ->
+            viewModel.confirmSort(sort)
+        },
+        openLoopDialog = {viewModel.openLoopDialog()},
+        loopSelect = {viewModel.loopSelect()},
         onBackClick = onBackClick,
-        onMenuClick = onMenuClick
+        onMenuClick = onMenuClick,
+        onSearchClick = {viewModel.onSearchClick()},
     )
+
+    DisposableEffect(Unit) {
+        onDispose {
+            viewModel.resetUiState()
+        }
+    }
 }
 
 
 @Composable
 private fun CourseRecommendationContent(
+    uiState: CourseRecommendationUiState,
+    onDistanceClick:()->Unit,
+    onDistanceClose:()->Unit,
+    onDistanceConfirm:(Int)->Unit,
+    onSortClick:()->Unit,
+    onSortClose:()->Unit,
+    onSortConfirm:(SortType)->Unit,
+    openLoopDialog:()->Unit,
+    loopSelect:(Boolean)->Unit,
     onBackClick:()->Unit,
-    onMenuClick:()->Unit
+    onMenuClick:()->Unit,
+    onSearchClick:()->Unit,
 ){
+    val textLoopFirst :String = if(uiState.isLoop) "왕복" else "편도"
+    val textLoopSecond :String = if(uiState.isLoop) "편도" else "왕복"
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
@@ -60,6 +106,9 @@ private fun CourseRecommendationContent(
     ) {
         Column(
         ){
+            Box(){
+
+            }
             TopBar(onBackClick = onBackClick, onMenuClick = onMenuClick)
             Box(
                 modifier = Modifier
@@ -72,14 +121,19 @@ private fun CourseRecommendationContent(
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
                         .padding(bottom = 30.dp)
+                        .clickable{onSearchClick()}
                 ){
                     Box(
                         contentAlignment = Alignment.Center,
                         modifier = Modifier
                             .height(60.dp)
                             .width(220.dp)
-                            .background(color = White,shape = RoundedCornerShape(10.dp))
-                            .border(width = 3.dp, color = PointColor,shape = RoundedCornerShape(10.dp))
+                            .background(color = White, shape = RoundedCornerShape(10.dp))
+                            .border(
+                                width = 3.dp,
+                                color = PointColor,
+                                shape = RoundedCornerShape(10.dp)
+                            )
                     ){
                         Text(
                             text = "코스 추천 받기",
@@ -92,9 +146,72 @@ private fun CourseRecommendationContent(
             }
 
             Spacer(modifier = Modifier.height(25.dp))
-            componentRecommend(text = "목표 러닝 거리", text2 = "3km")
-            componentRecommend(text = "거리 계산 방법", text2 = "왕복")
-            componentRecommend(text = "정렬 방법", text2 = "선택해주세요")
+            componentRecommend(text = "목표 러닝 거리", textInfo = "${uiState.goalDistance}km", onDistanceClick)
+            if(uiState.showLoop){
+                LoopDialog(
+                    textLoopFirst = textLoopFirst,
+                    textLoopSecond = textLoopSecond,
+                    loopSelect = loopSelect
+                )
+            }
+            else{
+                componentRecommend(text = "거리 계산 방법", textInfo = textLoopFirst, openLoopDialog)
+                componentRecommend(text = "정렬 방법", textInfo = "${uiState.currentSort.label}", onSortClick)
+            }
+        }
+    }
+
+
+    if (uiState.showDistanceDialog) {
+        DistanceGoalSettingDialog(
+            range = 0..100,
+            startNumber = (uiState.goalDistance/100 + 1),
+            onConfirm = onDistanceConfirm,
+            onDismiss = onDistanceClose
+        )
+    }
+}
+
+
+
+@Composable
+private fun LoopDialog(
+    textLoopFirst:String,
+    textLoopSecond:String,
+    loopSelect:(Boolean)->Unit
+){
+    Row(
+        modifier = Modifier
+            .padding(start = 18.dp, end = 18.dp, top = 10.dp, bottom = 10.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ){
+        Text(
+            text = "거리 계산 방법",
+            fontSize = 22.sp,
+            color = TextWhite,
+            modifier = Modifier.weight(1f)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .background(color = White, shape = RoundedCornerShape(5.dp))
+        ){
+            InfoText(
+                text = textLoopFirst,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable{loopSelect(true)}
+            )
+            Spacer(
+                modifier = Modifier.fillMaxWidth().height(1 .dp).background(color = Gray)
+            )
+            InfoText(
+                text = textLoopSecond,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable{loopSelect(false)}
+            )
         }
     }
 }
@@ -102,37 +219,50 @@ private fun CourseRecommendationContent(
 @Composable
 private fun componentRecommend(
     text:String ="",
-    text2:String = ""
+    textInfo:String = "",
+    onClick:()->Unit
 ){
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
             .padding(start = 18.dp, end = 18.dp, top = 10.dp, bottom = 10.dp)
+            .clickable { onClick()}
     ){
         Text(
             text = text,
             fontSize = 22.sp,
             color = TextWhite,
-            modifier = Modifier.weight(1f)
-        )
-        Column(
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .background(color = White, shape = RoundedCornerShape(5.dp))
                 .weight(1f)
-                .height(38.dp)
-                .padding(end = 10.dp)
-        ){
-            Text(
-                text = text2,
-                fontSize = 22.sp,
-                color = TextBlack,
-            )
-        }
+        )
+        InfoText(text = textInfo,
+            modifier = Modifier
+                .weight(1f)
+                .background(color = White, shape = RoundedCornerShape(5.dp))
+        )
     }
 }
+@Composable
+private fun InfoText(
+    text:String,
+    modifier:Modifier = Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .height(38.dp)
+            .padding(end = 10.dp)
+    ){
+        Text(
+            text = text,
+            fontSize = 22.sp,
+            color = TextBlack,
+        )
+    }
+}
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun MapHorizontalPager() {
@@ -176,7 +306,10 @@ private fun CourseRecommendationGoogleMap(
                     modifier = Modifier
                         .height(30.dp)
                         .width(70.dp)
-                        .background(color = White.copy(alpha = 0.5f), shape = RoundedCornerShape(5.dp))
+                        .background(
+                            color = White.copy(alpha = 0.5f),
+                            shape = RoundedCornerShape(5.dp)
+                        )
                 ){
                     Text(
                         text = "코스 ${pageIndex}",
@@ -193,5 +326,8 @@ private fun CourseRecommendationGoogleMap(
 @Preview
 @Composable
 private fun PreviewCourseRecommendationContent(){
-    CourseRecommendationContent({}, {})
+    CourseRecommendationContent(
+        uiState = CourseRecommendationUiState(),
+        {},{},{},{}, {},{},{},{},{},{},{},
+    )
 }
---
+++ app/src/main/java/com/example/runup/viewmodel/CourseRecommendationViewModel.kt
@@ -0,0 +1,121 @@
+package com.example.runup.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.runup.domain.model.SortType
+import com.example.runup.domain.repository.LocationRepository
+import com.example.runup.domain.usecase.GetUserGoalUseCase
+import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+data class CourseRecommendationUiState(
+    val goalDistance: Int = 0,
+    val currentLocation: LatLng? = null,   //현재 위치
+    val currentSort: SortType = SortType.DISTANCE,
+    val isLoop: Boolean = true,
+    val showLoop: Boolean = false,
+    val showDistanceDialog: Boolean = false,
+    val showSortDialog: Boolean = false
+)
+@HiltViewModel
+class CourseRecommendationViewModel @Inject constructor(
+    private val getUserGoalUseCase: GetUserGoalUseCase,
+    private val locationRepository: LocationRepository,
+): ViewModel(){
+    private val _uiState = MutableStateFlow(CourseRecommendationUiState())
+    val uiState: StateFlow<CourseRecommendationUiState> = _uiState
+
+    init {
+        loadUserGoal()
+        observeCurrentLocation()
+        startCurrentLocationTracking()
+    }
+
+    private fun loadUserGoal() {
+        viewModelScope.launch {
+            getUserGoalUseCase().collectLatest { goal ->
+                goal?.let { (distance) ->
+                    _uiState.update {
+                        it.copy(
+                            goalDistance = distance
+                        )
+                    }
+                }
+            }
+        }
+    }
+    private fun observeCurrentLocation() {
+        viewModelScope.launch {
+            locationRepository.currentLocation.collectLatest { location ->
+                _uiState.update {
+                    it.copy(
+                        currentLocation = location?.let {
+                            LatLng(it.latitude, it.longitude)
+                        }
+                    )
+                }
+            }
+        }
+    }
+    fun resetUiState() {
+        _uiState.value = CourseRecommendationUiState()
+    }
+
+    fun onSearchClick(){
+        // 여기서 uiState의 goalDistance, currentSort, isLoop 를 usecase에 넘기는 함수 작성
+    }
+
+    fun openLoopDialog(){
+        _uiState.update { it.copy(showLoop = true) }
+    }
+
+    fun loopSelect(isFirst: Boolean = true){
+        if(isFirst) _uiState.update { it.copy(showLoop = false) }
+        else _uiState.update { it.copy(isLoop = !_uiState.value.isLoop, showLoop = false) }
+    }
+
+    // [1] 단순 위치 추적 시작 (GPS 서비스 ON)
+    fun startCurrentLocationTracking() {
+        locationRepository.startTracking()
+    }
+
+    fun stopCurrentLocationTracking() {
+        locationRepository.stopTracking()
+    }
+
+    // 목표 설정 함수들
+    fun openDistanceDialog() {
+        _uiState.update { it.copy(showDistanceDialog = true) }
+    }
+    fun closeDistanceDialog() {
+        _uiState.update { it.copy(showDistanceDialog = false) }
+    }
+
+    fun openSortDialog() {
+        _uiState.update { it.copy(showSortDialog = true) }
+    }
+    fun closeSortDialog() {
+        _uiState.update { it.copy(showSortDialog = false) }
+    }
+
+
+    fun confirmDistance(distanceKm: Int) {  //이 함수에서 db에 목표거리 저장 (distanceMeter)
+        val distanceMeter:Int = distanceKm*100
+        _uiState.update {
+            it.copy(showDistanceDialog = false, goalDistance = distanceMeter)
+        }
+    }
+
+    fun confirmSort(sortType: SortType) {  //이 함수에서 db에 목표거리 저장 (distanceMeter)
+        _uiState.update {
+            it.copy(showSortDialog = false, currentSort = sortType)
+        }
+    }
+}
